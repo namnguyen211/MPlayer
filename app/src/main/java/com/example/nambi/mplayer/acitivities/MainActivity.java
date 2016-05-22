b6884e1621068742c768ae18f3a5e164fd6c9dd8
nambi <nbnguyen211@gmail.com>
2016-05-22 17:23:49 +0700

    customview

--- app/src/main/java/com/example/nambi/mplayer/acitivities/MainActivity.java
+++ app/src/main/java/com/example/nambi/mplayer/acitivities/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.nambi.mplayer.acitivities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 import com.example.nambi.mplayer.R;
 import com.example.nambi.mplayer.adapters.ElementListAdapter;
@@ -20,11 +25,14 @@ public class MainActivity extends AppCompatActivity {
     ElementListAdapter listAdapter;
     List<BaseListElement> listElements;
     View listHeader,listFooter;
+    SimpleCursorAdapter simpleAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        simpleAdapter = new SimpleCursorAdapter(this,android.R.layout.simple_list_item_1,)
 
         listView = (ListView) findViewById(R.id.listView);
         listElements = new ArrayList<>();
@@ -54,5 +62,15 @@ public class MainActivity extends AppCompatActivity {
         listView.addHeaderView(listHeader);
         listView.addFooterView(listFooter);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.menu_search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        return true;
     }
 }
